3087a9400eb05304d0a3b8c9c5de99774913bf32
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-03-10 23:43:11 +0800

    feat(scripts): add css variables and unocss config builders

---
+++ scripts/build-variables.ts
@@ -0,0 +1,50 @@
+import lightTokens from '../assets/tokens/light.json'
+import darkTokens from '../assets/tokens/dark.json'
+import {
+  getSdOverloadedConfig,
+  baseStyleDictionary,
+  type TokensConfig,
+  WEB_PLATFORM,
+} from './shared'
+
+const configs: TokensConfig[] = [
+  {
+    brand: 'light',
+    selector: ':root',
+    tokens: lightTokens,
+    buildExtraFiles: {
+      uno: true,
+    },
+  },
+  {
+    brand: 'dark',
+    selector: ':root.dark',
+    tokens: darkTokens,
+  },
+]
+
+async function run() {
+  console.log('')
+  console.log('Build started...')
+
+  configs.map((config) => {
+    console.log('')
+    console.log(`Processing:  [Web] [${config.brand}]`)
+
+    // https://amzn.github.io/style-dictionary/#/quick_start?id=node
+    const overloaded = getSdOverloadedConfig(config)
+    const sd = baseStyleDictionary.extend(overloaded)
+    sd.buildPlatform(WEB_PLATFORM)
+
+    console.log('Done.')
+    console.log('')
+  })
+
+  console.log('')
+  console.log('Build completed!')
+  console.log('')
+}
+
+run().catch((e) => {
+  console.log(e)
+})

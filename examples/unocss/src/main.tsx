356a8effa82522ec106d1bb2137cad0a39ce9aa1
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-03-10 23:45:40 +0800

    feat(examples): add a react example to debug unocss theme config

---
+++ examples/unocss/src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import 'virtual:uno.css'
+import '@fn/theme/light-variables.css'
+import '@fn/theme/dark-variables.css'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
---
+++ examples/unocss/uno.config.ts
@@ -0,0 +1,4 @@
+import { defineConfig } from '@bassist/uno'
+import { theme } from '@fn/theme'
+
+export default defineConfig({ theme })

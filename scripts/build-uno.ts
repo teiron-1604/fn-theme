036ae5a2db3c62158fa128f393ff6b2e4a7fc6d9
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-03-10 23:44:03 +0800

    feat(scripts): add unocss config bundle builder

---
+++ scripts/build-uno.ts
@@ -0,0 +1,6 @@
+import unoConfig from '../assets/variables/uno-config.json'
+import type { Theme } from '@unocss/preset-wind'
+
+const theme: Theme = unoConfig
+
+export { theme }
---
+++ tsup.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: {
+    index: 'scripts/build-uno.ts',
+  },
+  format: ['esm', 'cjs'],
+  outExtension({ format }) {
+    return {
+      js: `.${format === 'esm' ? 'mjs' : 'cjs'}`,
+    }
+  },
+  publicDir: 'public',
+  dts: true,
+  minify: true,
+  splitting: false,
+  clean: true,
+})

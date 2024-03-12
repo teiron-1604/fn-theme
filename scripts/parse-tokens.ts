676d78359975fb6b1c88ec5470c9a31cbc861516
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-03-12 10:38:38 +0800

    feat(scripts): add px unit to value of number tokens

--- scripts/parse-tokens.ts
+++ scripts/parse-tokens.ts
@@ -5,12 +5,36 @@ import {
   parsedFigmaTokensFile,
 } from './shared'
 
-function setAnotherTokenType(config: Record<string, any>, anotherType: string) {
+function setAnotherTokenType(
+  tokenConfig: Record<string, any>,
+  anotherType: string,
+) {
+  const newTokenConfig: Record<string, any> = {}
+  for (const key in tokenConfig) {
+    if (Object.prototype.hasOwnProperty.call(tokenConfig, key)) {
+      newTokenConfig[key] = tokenConfig[key]
+      newTokenConfig[key].type = anotherType
+    }
+  }
+  return newTokenConfig
+}
+
+function addUnit(tokenConfig: Record<string, any>) {
+  for (const key in tokenConfig) {
+    if (Object.prototype.hasOwnProperty.call(tokenConfig, key)) {
+      if (tokenConfig[key]?.type === 'number') {
+        tokenConfig[key].value = `${tokenConfig[key].value}px`
+      }
+    }
+  }
+}
+
+function addUnitToNumberTokens(config: Record<string, any>) {
   const newConfig: Record<string, any> = {}
   for (const key in config) {
     if (Object.prototype.hasOwnProperty.call(config, key)) {
       newConfig[key] = config[key]
-      newConfig[key].type = anotherType
+      addUnit(newConfig[key])
     }
   }
   return newConfig
@@ -24,7 +48,6 @@ async function run() {
     .replace(prefixPattern, '')
     .replace(/\/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*\/+/g, separator)
     .replace('"radius"', '"borderRadius"')
-    .replace('"blur"', '"blur"')
     .replace(/(radius|blur)-/g, '')
 
   const fragments = replaced
@@ -38,15 +61,19 @@ async function run() {
     })
     .filter(Boolean)
 
-  const [light, dark, foundations, global] = fragments
+  const [_light, _dark, _foundations, _global] = fragments
+
+  const light = addUnitToNumberTokens(_light)
+  const dark = addUnitToNumberTokens(_dark)
+  const foundations = addUnitToNumberTokens(_foundations)
+  const global = addUnitToNumberTokens(_global)
 
   light.text = setAnotherTokenType(light.text, 'textColor')
-  dark.text = setAnotherTokenType(dark.text, 'textColor')
-
   light.bg = setAnotherTokenType(light.bg, 'backgroundColor')
-  dark.bg = setAnotherTokenType(dark.bg, 'backgroundColor')
-
   light.border = setAnotherTokenType(light.border, 'borderColor')
+
+  dark.text = setAnotherTokenType(dark.text, 'textColor')
+  dark.bg = setAnotherTokenType(dark.bg, 'backgroundColor')
   dark.border = setAnotherTokenType(dark.border, 'borderColor')
 
   const json = {

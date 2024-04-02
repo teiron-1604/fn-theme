64fa4c81e8b968ff0d00fcb035615219259baa41
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-04-03 00:00:07 +0800

    chore(example): add tailwind example

--- examples/tailwind/vite.config.ts
+++ examples/tailwind/vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import unocss from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 2816,
+    port: 2918,
   },
-  plugins: [react(), unocss()],
+  plugins: [react()],
 })
